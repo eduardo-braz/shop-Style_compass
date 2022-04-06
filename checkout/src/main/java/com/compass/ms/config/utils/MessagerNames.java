7b7af8e2f6cb5f6367a53561006b651c9a2a6fca
Eduardo Braz de Campos <eduardo.braz@compasso.com.br>
2022-04-06 08:42:41 -0300

    Configuração inicial do RabbitMQ

---
+++ checkout/src/main/java/com/compass/ms/config/utils/MessagerNames.java
@@ -0,0 +1,7 @@
+package com.compass.ms.config.utils;
+
+public interface QueueNames {
+    public static final String CHECKOUT_QUEUE = "CheckoutQueue";
+    public static final String CHECKOUT_EXCHANGE = "CheckoutExchange";
+    public static final String CHECKOUT_ROUTER_KEY = "CheckoutRouterKey";
+}
